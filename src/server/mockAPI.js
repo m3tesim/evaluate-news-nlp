3ed1d29a63954a52c0665a4824661236534fbab9
Your NameMoatasem <m3tsmsh@gmail.com>
2022-01-02 23:57:15 +0200

    install nodemon and dotenv

--- src/server/mockAPI.js
+++ src/server/mockAPI.js
@@ -1,3 +1,11 @@
+
+// using dotenv to hide the API key
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(`Your API key is ${process.env.API_KEY}`);
+
+
 let json = {
     'title': 'test json response',
     'message': 'this is a message',
